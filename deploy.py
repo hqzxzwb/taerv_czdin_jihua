8d29ba0355186bfe6b7a24c5fb498e93dc904ace
osfans <waxaca@163.com>
2019-07-18 13:34:23 +0800

    Fix path

--- deploy.py
+++ deploy.py
@@ -23,57 +23,69 @@ def get_letters(dirs):
     lines.append("### %s\n" % letters)
     return lines
 
-def lower_er(pys, word):
+def lower_er(py0, word):
     """儿尾变小"""
-    if "r" in pys:
+    syllables = py0.split(" ")
+    if "r" in syllables:
         words = list(word)
         index = 0
-        while "r" in pys[index:]:
-            index = pys.index("r", index)
+        while "r" in syllables[index:]:
+            index = syllables.index("r", index)
             if words[index] == "儿":
                 words[index] = "<sub>儿</sub>"
             index += 1
         word = "".join(words)
     return word
 
-def check_path(path, py, word):
+def check_path(path, py0, word):
     """检查词语的文件名是否正确"""
-    py = re.sub(r"\d", "", py.replace(" ", "_"))
-    if not path.endswith("%s/%s.md" % (py[0], py)):
-        print("【%s】不属于%s" %(word, path))
+    py0 = re.sub(r"\d", "", py0.replace(" ", "_"))
+    if not path.endswith("%s/%s.md" % (py0[0], py0)):
+        print("【%s】不属于 %s" %(word, path))
+
+def parse_cont(cont):
+    """解析词条"""
+    cont = cont.strip()
+    fields = re.split("\n+", cont)
+    word = fields[0].lstrip("#").strip()
+    pinyin = fields[1].strip()
+    meaning = ""
+    show_order = cont.count("\n-") > 1
+    meaning_count = 0
+    example_count = 0
+    for line in fields[2:]:
+        if line.startswith("-"):
+            example_count = 0
+            meaning_count += 1
+            if show_order:
+                meaning += ORDERS[meaning_count]
+            meaning += " " + line.replace("-", "").strip()
+        else:
+            example_count += 1
+            meaning += "｜" if example_count > 1 else "："
+            example = line.strip().replace("-", "").strip()
+            if "/" in example:
+                example = example.replace("/", "<sub>")+"</sub>"
+            meaning += example
+    return [word, pinyin, meaning]
+
+def write_index():
+    """生成主页"""
+    dirs = sorted(glob.glob("?"))
+    lines = get_letters(dirs)
+    for path in dirs:
+        lines.append("## %s\n" % path.upper())
+        for fname in sorted(glob.glob(path+"/*.md")):
+            conts = open(fname).read()
+            for cont in re.findall(r"#[^#]+", conts):
+                word, pinyin, meaning = parse_cont(cont)
+                py0 = pinyin.split(",")[0]
+                link = LINK_FORMAT % (fname, word)
+                out = "【[%s](%s)】`%s` %s  \n" % (lower_er(py0, word), link, pinyin, meaning)
+                lines.append(out)
+                check_path(fname, py0, word)
+        lines.append("### [▲](#音序检索)\n")
+    open("docs/index.md", "w").writelines(lines)
 
 write_config()
-DIRS = sorted(glob.glob("?"))
-LINES = get_letters(DIRS)
-
-for d in DIRS:
-    LINES.append("## %s\n" % d.upper())
-    for f in sorted(glob.glob(d+"/*.md")):
-        conts = open(f).read()
-        for cont in conts.split("#"):
-            if cont:
-                cont = cont.strip()
-                fs = re.split("\n+", cont)
-                py = fs[1].split(",")[0]
-                check_path(f, py, fs[0])
-                link = LINK_FORMAT % (f, fs[0])
-                out = "【[%s](%s)】`%s` " % (lower_er(py.split(" "), fs[0]), link, fs[1])
-                meanings = cont.count("\n-")
-                meaning = 0
-                example_count = 0
-                for line in fs[2:]:
-                    if line.startswith("-"):
-                        meaning += 1
-                        if meanings > 1:
-                            out += ORDERS[meaning]
-                        out += " " + line.replace("-", "").strip()
-                    else:
-                        example_count += 1
-                        out += "｜" if example_count > 1 else "："
-                        example = line.strip().replace("-", "").strip()
-                        if "/" in example:
-                            example = example.replace("/", "<sub>")+"</sub>"
-                        out += example
-                LINES.append(out+"  \n")
-    LINES.append("### [▲](#音序检索)\n")
-open("docs/index.md", "w").writelines(LINES)
+write_index()
