10fff34552ed224298a02c528923acee814c2190
osfans <waxaca@163.com>
2020-06-22 14:21:47 +0800

    script

--- deploy.py
+++ deploy.py
@@ -40,7 +40,7 @@ def lower_er(py0, word):
 
 def get_path(py0):
     """获取文件路径"""
-    py0 = re.sub(r"\d", "", re.sub("[—，（） ]+", "_", py0))
+    py0 = re.sub(r"\d", "", re.sub("[^a-z0-8]+", "_", py0))
     py0 = py0.rstrip("_")
     return os.path.join(py0[0], "%s.md" % py0)
 
